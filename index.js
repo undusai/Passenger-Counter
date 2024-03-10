18ed774c8430bdfacb9e0daeb48c1d2729dfe984
undusai <161234837+undusai@users.noreply.github.com>
2024-03-10 20:00:56 +0530

    Add files via upload
    Passenger counter for counter people in a queue

---
+++ index.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+console.log(countEl)
+console.log(saveEl)
+let count = 0
+
+function increment(){
+    count += 1
+    countEl.textContent = count
+}
+
+function save(){
+    let saveCount = count + " - "
+    saveEl.textContent += saveCount
+    countEl.textContent = 0
+    count = 0
+}
+
